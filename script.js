71eb91b4e7dec8d9a6fa4621d733c393fb5d6d80
Julia Georgiew <java2011@me.com>
2023-03-18 22:43:46 +0100

    still problems

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ async function init() {
     setURL('https://julia-georgiew.developerakademie.net/smallest_backend_ever');
     await downloadFromServer();
     projects = JSON.parse(backend.getItem('projects')) || [];
+    console.log('1' , projects);
 }
 function addProject(project) {
     projects.push(project);
@@ -25,53 +26,76 @@ function readProject(){
     let project = {
         'title' : title.value,
         'description': description.value,
+        'image': image.value,
         'startDate' : startDate.value,
         'firstSubmission': firstSubmission.value
     }
     addProject(project);
 }
 function renderProjects(){
-    console.log('render Projekte');
+    console.log('2 render Projekte');
    let mainContainer = document.getElementById('main');
-   for (let i = 0; i < 1; i++) {
+   for (let i = 0; i < projects.length; i++) {
     
     mainContainer.innerHTML += `
     <div id="card" class="container-fluit">
     <div class="card" style="width: 18rem;">
-        <img src="assets/img/sakura.png" class="card-img-top">
+        <img src="assets/img/${projects[i].image}" class="card-img-top">
         <div class="card-body">
-          <h5 class="card-title">${projects[i]['title']}</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 class="card-title">${projects[i].title}</h5>
+          <p class="card-text">${projects[i].description}.</p>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">Startdatum: ${projects[i].startDate}</li>
+          <li class="list-group-item">Abgegeben am: ${projects[i].firstSubmission}</li>
+          <li class="list-group-item">Bearbeitungszeit: ${calculateDuration(i)} Tage</li>
         </ul>
         <div class="card-body">
           <a href="#" class="card-link">Card link</a>
-          <a href="#" class="card-link">Another link</a>
+          <a onclick="deleteProject(${i})" href="#" class="card-link">löschen</a>
         </div>
       </div>
 </div>`;
+calculateDuration(i);
+deleteProject(i);
    }
   
 }
 
+async function deleteProject(i) {
+    await backend.deleteItem('title');
+    await backend.deleteItem('description');
+    await backend.deleteItem('startDate');
+    await backend.deleteItem('firstSubmission');
+  }
+
+function calculateDuration(i){
+   let second = new Date(projects[i].firstSubmission);
+    let first = new Date(projects[i].startDate);
+
+    let duration = second.getTime() - first.getTime();
+   duration = duration / 86400000;
+       return duration
+}
+
+function delay(){
+    setTimeout(renderProjects, 300);
+}
 function showInput(){
-    console.log('addProject');
+  
     let newContainer = document.getElementById('container');
     newContainer.innerHTML = `
-<div id="newProject" class="container-fluit d-none">
+<div id="newProject" class="container-fluit">
     <div>
-    
+    <form onsubmit="readProject()">
         <label for="title">Projektname</label>
-        <input id="title" class="form-control form-control-sm" type="text" placeholder="Projektname" aria-label=".form-control-sm example">
+        <input id="title" class="form-control form-control-sm" type="text" placeholder="Projektname" aria-label=".form-control-sm example" required>
        
-        <input id="description" class="form-control form-control-sm" type="text" placeholder="Beschreibung" aria-label=".form-control-sm example">
-        <input id="startDate" class="form-control form-control-sm" type="date" placeholder="Modulstart" aria-label=".form-control-sm example">
-        <input id="firstSubmission" class="form-control form-control-sm" type="date" placeholder="Eingereicht am" aria-label=".form-control-sm example">
-        <button onclick="readProject()">speichern</button>
+        <input id="description" class="form-control form-control-sm" type="text" placeholder="Beschreibung" aria-label=".form-control-sm example" required>
+        <input id="startDate" class="form-control form-control-sm" type="date" placeholder="Modulstart" aria-label=".form-control-sm example" required>
+        <input id="firstSubmission" class="form-control form-control-sm" type="date" placeholder="Eingereicht am" aria-label=".form-control-sm example" required>
+        <button>speichern</button>
+        </form>
     </div>
 </div>`;
 }
